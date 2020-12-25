eb8b5c83453b5c831f3dff97208a5b8e9a1e0cc9
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-25 21:31:52 +0000

    First try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello, I am Brunao.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    }
+  };
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          {this.state.string}
+          </p>
+          <button onClick={() => this.setState({string: 'Oi cara'})}>Change text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
