3a24defc8f5313fc602829c5f0db4da43c7115bb
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-25 23:59:20 +0000

    First component

--- src/App.js
+++ src/App.js
@@ -7,32 +7,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
-    }
-  };
+      monsters: []
+    };
+  }
 
-
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()) //this line takes the response of the fetch and converts it into a json
+      .then(users => this.setState({ monsters: users}));
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          {this.state.string}
-          </p>
-          <button onClick={() => this.setState({string: 'Oi cara'})}>Change text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name }</h1>)
+        }
       </div>
     );
   }
